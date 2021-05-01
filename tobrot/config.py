93d51fc58a34f9fccd252ea606bf87e97ec20e8e
MASTER <83451112+MASTER-JD@users.noreply.github.com>
2021-05-01 14:51:01 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,20 @@
+from tobrot.sample_config import Config
+
+#read readme too before filling these stuffs
+
+class Config(Config):
+    TG_BOT_TOKEN= "1787279802:AAFghBXTJkd_XQkSEEDj7K5tEy9uYTgyDw0" #imp
+    APP_ID = 3891645 #imp
+    API_HASH = "b675f9d5fc0af373978e4b5363288430" #imp
+    AUTH_CHANNEL = [-1001160005579, 841529957] #imp replace your_id with your id from telegram or delete
+    INDEX_LINK = "
+    GLEECH_COMMAND = "gleech@ThorleechBot"
+    YTDL_COMMAND = 'ytdl@ThorleechBot'
+    TELEGRAM_LEECH_COMMAND_G = "leech@ThorleechBot"
+    CLONE_COMMAND_G = "gclone"
+    PYTDL_COMMAND_G = "pytdl"
+    DESTINATION_FOLDER = "Gdrive bot"
+    LEECH_COMMAND = "leech@Gdrive bot"
+    CANCEL_COMMAND_G = "cancel@ThorleechBot"
+    RCLONE_CONFIG = """"""
+    #put your config(replacing new lines with \n) in triple quote like above: """<your one liner config>"""

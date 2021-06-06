59005961bed05c7fadd217b9059579d84168a2dd
MASTER <83451112+MASTER-JD@users.noreply.github.com>
2021-06-06 15:00:42 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -3,18 +3,18 @@ from tobrot.sample_config import Config
 #read readme too before filling these stuffs
 
 class Config(Config):
-    TG_BOT_TOKEN= "1787279802:AAFghBXTJkd_XQkSEEDj7K5tEy9uYTgyDw0" #imp
+    TG_BOT_TOKEN= "1793051131:AAG_Uhu9FbYGdSQLJ7AQ3LaUT7sn29cislw" #imp
     APP_ID = 3891645 #imp
     API_HASH = "b675f9d5fc0af373978e4b5363288430" #imp
-    AUTH_CHANNEL = [-1001160005579, 841529957] #imp replace your_id with your id from telegram or delete
-    INDEX_LINK = "
-    GLEECH_COMMAND = "gleech@ThorleechBot"
-    YTDL_COMMAND = 'ytdl@ThorleechBot'
-    TELEGRAM_LEECH_COMMAND_G = "leech@ThorleechBot"
-    CLONE_COMMAND_G = "gclone"
-    PYTDL_COMMAND_G = "pytdl"
+    AUTH_CHANNEL = [-1001228080139, 841529957] #imp replace your_id with your id from telegram or delete
+    INDEX_LINK = "https://jd.jacksparrow063.workers.dev"
+    GLEECH_COMMAND = "gleech@JDgt01_bot"
+    YTDL_COMMAND = 'ytdl@JDgt01_bot'
+    TELEGRAM_LEECH_COMMAND_G = "leech@JDgt01_bot"
+    CLONE_COMMAND_G = "gclone@JDgt01_bot"
+    PYTDL_COMMAND_G = "pytdl@JDgt01_bot"
     DESTINATION_FOLDER = "Gdrive bot"
-    LEECH_COMMAND = "leech@Gdrive bot"
-    CANCEL_COMMAND_G = "cancel@ThorleechBot"
+    LEECH_COMMAND = "leech@JDgt01_bot"
+    CANCEL_COMMAND_G = "cancel@JDgt01_bot"
     RCLONE_CONFIG = """"""
     #put your config(replacing new lines with \n) in triple quote like above: """<your one liner config>"""

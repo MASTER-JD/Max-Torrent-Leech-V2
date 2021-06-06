ea4b0728ef273377ac163227568cbacba73a7253
MASTER <83451112+MASTER-JD@users.noreply.github.com>
2021-06-06 15:42:08 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -1,12 +1,15 @@
 from tobrot.sample_config import Config
+
 #Fill your all data, read readme for reference
 
 class Config(Config):
-	TG_BOT_TOKEN= "1787279802:AAFghBXTJkd_XQkSEEDj7K5tEy9uYTgyDw0"
+	TG_BOT_TOKEN= "1793051131:AAG_Uhu9FbYGdSQLJ7AQ3LaUT7sn29cislw"
 	APP_ID = 3891645
 	API_HASH = "b675f9d5fc0af373978e4b5363288430"
 	OWNER_ID = "841529957" #ID of bot owner
-	AUTH_CHANNEL = [-1001160005579]
-	DESTINATION_FOLDER = "" #Name of your folder read readme
-	RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
+	AUTH_CHANNEL = [-1001228080139]
+	DESTINATION_FOLDER = "TorrentLeech-Gdrive" #Name of your folder read readme
+	RCLONE_CONFIG = """type = drive\scope = drive\token = {"access_token":"ya29.a0AfH6SMBZ7NEwsbMl7t0a-S-BTPqXTVNAoDUSR0gT-OOVLNtGiFYLTm7oVgff59F3lt1Qh5rZzbl-O45LS2TSV1bkD3-WIrBk3An--iEzoysJnpe1cMcxMM_TJ6YGXKjP6oKdXY-xaCipEiLKPyjn8p1ylP0V","token_type":"Bearer","refresh_token":"1//0gaiqsHtrvKK9CgYIARAAGBASNwF-L9IrNIHAHmyrc9JC9lGZGE2zqN8h_SFm8P_Zrw-RpKPqw7LyJIj8pDyGyNhdH1HCpIQn0-4","expiry":"2021-06-03T13:33:13.781245+05:30"}"""
+	team_drive = "0AF49E9jk7kbXUk9PVA"
+        root_folder_id = "19AGiAyn33rtHjeoRp0-sClljINOT8sKo"
 	#fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
